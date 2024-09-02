bd5d8b2246e40530fcbd9295a3aca76833b2ffc8
noxinput <jordyvanwete@gmail.com>
2024-09-02 12:34:29 +0200

    v0.1

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+const expandButton = document.querySelector('header button') // hiermee zorg je ervor dat je expandbutton kan noemen, hier selecteert hij de button in de header
+expandButton.addEventListener('click', expand) // Hier zorg je ervoor dat de button "luistert" naar een click waardoor de functie expand uitgevoerd wordt
 
 function expand () {
-  document.body.classList.toggle('expand')
+  document.body.classList.toggle('expand') // Hier wordt de class expand getoggled op de body, waardoor deze een andere styling krijgt
 }
